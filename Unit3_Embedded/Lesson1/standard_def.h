11031da75408ac4d34d3eea01cdc4e4e7cee8b42
CatherineDemian <138332588+CatherineDemian@users.noreply.github.com>
2024-10-21 22:35:00 +0300

    Add files via upload

---
+++ Unit3_Embedded/Lesson1/standard_def.h
@@ -0,0 +1,36 @@
+/*
+ ============================================================================
+ Name        : Lesson.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+typedef int uint32;
+typedef float ufloat32;
+typedef char uint8;
+typedef short uint16;
+typedef double ufloat64;
+typedef long long uint64;
+typedef long double ufloat80;
+
+typedef signed char sint8;
+typedef signed int sint32;
+typedef signed short sint16;
+typedef signed long long sint64;
+
+
+typedef volatile int vuint32;
+typedef volatile float vufloat32;
+typedef volatile char vuint8;
+typedef volatile short vuint16;
+typedef volatile double vufloat64;
+typedef volatile long long vuint64;
+typedef volatile long double vufloat80;
+
+typedef volatile signed char vsint8;
+typedef volatile signed int vsint32;
+typedef volatile signed short vsint16;
+typedef volatile signed long long vsint64;
+
